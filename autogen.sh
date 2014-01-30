eb1e8300767d605fc19abb42f6d404f6977d9980
Murray Cumming <murrayc@murrayc.com>
2014-01-30 10:42:18 +0100

    Convert from gnome-doc-utils to yelp-utils.

--- autogen.sh
+++ autogen.sh
@@ -3,7 +3,6 @@ test -n "$srcdir" || srcdir=`dirname "$0"`
 test -n "$srcdir" || srcdir=.
 (
   cd "$srcdir" &&
-  gnome-doc-prepare --automake --copy --force &&
   autoreconf --force --install --verbose
 ) || exit
 test -n "$NOCONFIGURE" || "$srcdir/configure" --enable-maintainer-mode "$@"
